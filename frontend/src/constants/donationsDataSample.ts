15fb124d86ecd61504bcededb86cacc02ba3b0b7
pahu2353 <pahu2353@gmail.com>
2024-05-29 20:59:38 -0400

    basic table and filter created w/ chakra
    Co-authored-by: Jordan Kok Ee Hsin <eehsinkok777@gmail.com>

---
+++ frontend/src/constants/donationsDataSample.ts
@@ -0,0 +1,74 @@
+const donationsData = [
+    {
+        Cause: "Fighting Child Poverty",
+        Date: "2024-01-15",
+        Time: "12:00",
+        Amount: 150,
+        Frequency: "Monthly"
+    },
+    {
+        Cause: "Fighting Child Poverty",
+        Date: "2024-02-15",
+        Time: "12:00",
+        Amount: 150,
+        Frequency: "Monthly"
+    },
+    {
+        Cause: "Restoring the Environment",
+        Date: "2024-03-22",
+        Time: "15:30",
+        Amount: 200,
+        Frequency: "One-time"
+    },
+    {
+        Cause: "Malaria Prevention",
+        Date: "2024-05-18",
+        Time: "09:45",
+        Amount: 300,
+        Frequency: "Yearly"
+    },
+    {
+        Cause: "Healthcare Support",
+        Date: "2024-04-20",
+        Time: "11:00",
+        Amount: 250,
+        Frequency: "One-time"
+    },
+    {
+        Cause: "Education for All",
+        Date: "2024-07-29",
+        Time: "10:00",
+        Amount: 100,
+        Frequency: "Monthly"
+    },
+    {
+        Cause: "Animal Welfare",
+        Date: "2024-08-05",
+        Time: "16:15",
+        Amount: 75,
+        Frequency: "Monthly"
+    },
+    {
+        Cause: "Water Access",
+        Date: "2024-12-12",
+        Time: "13:20",
+        Amount: 180,
+        Frequency: "One-time"
+    },
+    {
+        Cause: "Climate Change Action",
+        Date: "2024-11-30",
+        Time: "14:00",
+        Amount: 300,
+        Frequency: "Monthly"
+    },
+    {
+        Cause: "Disaster Relief",
+        Date: "2024-10-16",
+        Time: "09:00",
+        Amount: 500,
+        Frequency: "Yearly"
+    }
+];
+
+export default donationsData;
