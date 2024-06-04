5bd9e03e95cc4f57e4179614d9101a4ca14bf0fe
Jason Le <lej2353@gmail.com>
2024-06-03 21:02:19 -0400

    creating checkout session endpoint

---
+++ backend/typescript/services/interfaces/stripeService.ts
@@ -0,0 +1,19 @@
+import { DonationDTO, Recurrence } from "../../types";
+
+interface IStripeService {
+  /**
+   * Create a checkout session for a one time payemnt
+   * @param user_id user's id
+   * @param amount the amount the user is donating toward this cause in the donation
+   * @param cause_id the id of the cause the donation is associated with
+   * @returns the newly created session url
+   * @throws Error if
+   */
+  createCheckoutSessionPayment(
+    user_id: string,
+    amount: number,
+    cause_id: number,
+  ): Promise<string>;
+}
+
+export default IStripeService;
