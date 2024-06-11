9f34f1dc021b2075c891133a4baf903e4f0d718c
roskzhu <roskenzhu@gmail.com>
2024-06-10 22:18:21 -0400

    stripe webhooks with string cause ids

--- backend/typescript/rest/donationsRoutes.ts
+++ backend/typescript/rest/donationsRoutes.ts
@@ -1,9 +1,15 @@
 // import { Prisma } from "@prisma/client";
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import prisma from "../prisma";
 import DonationService from "../services/implementations/donationService";
 import IDonationService from "../services/interfaces/donationService";
 import { getErrorMessage } from "../utilities/errorUtils";
+import Stripe from 'stripe';
+import bodyParser from 'body-parser';
+
+const stripe = new Stripe('your-stripe-secret-key', {
+  apiVersion: '2024-04-10',
+});
 
 const donationService: IDonationService = new DonationService();
 
@@ -49,6 +55,55 @@ donationRouter.post("/give", async (req: any, res: any) => {
     res.status(200).json(newDonation);
   } catch (error) {
     res.status(500).json({ error: getErrorMessage(error) });
+  }
+});
+
+const endpointSecret = "whsec_5b54bda18bea945a75f7bc81720b63f384eefcc77aba1750f4446f73d4ee7f86";
+
+// Stripe webhook to handle payment events
+donationRouter.post("/webhook", bodyParser.raw({ type: 'application/json' }), async (request: Request, res: Response) => {
+  const sig = request.headers['stripe-signature'];
+
+  if (sig === undefined) {
+    return res.status(400).send("Missing Stripe signature header");
+  }
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (error) {
+    res.status(500).json({ error: getErrorMessage(error) });
+  }
+
+  // Handle the event
+  if (event && event.type === 'checkout.session.completed') {
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    // Assuming your session contains the necessary information
+    const userId = session.client_reference_id;
+    const amount = session.amount_total;
+    // const causeId = session.metadata ? session.metadata.causeId : null; // Assign null if metadata is null
+    const isRecurring = session.mode;
+    const confirmationEmailSent = true; // Set as needed
+
+    // Check if metadata is null before accessing causeId
+    const causeIdString = session.metadata ? session.metadata.causeId : null; // Assign null if metadata is null
+    
+    // Convert causeIdString to a number
+    const causeId: number | null = causeIdString ? parseInt(causeIdString, 10) : null;
+
+    try {
+      if (userId === null || amount === null || causeId === null || isRecurring === null || confirmationEmailSent === null) {
+        throw new Error("User ID not found in session");
+      }
+
+      await donationService.createDonation(userId, amount, causeId, isRecurring, confirmationEmailSent);
+      res.status(200).send({ received: true });
+    } catch (error) {
+      res.status(500).json({ error: getErrorMessage(error) });
+    }
+  } else {
+    res.status(400).end();
   }
 });
 
