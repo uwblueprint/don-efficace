45538394a214bc7d6289d665e57ce57be3d810a2
Leo Zhang <leozhang53@gmail.com>
2024-06-20 21:01:52 -0400

    fixed navbar spacing and link color - reorganized/simplified dashboard/donations/account stack

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -6,7 +6,6 @@ import {
   Stack,
   Link,
   useColorModeValue,
-  useBreakpointValue,
 } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 
@@ -14,50 +13,27 @@ interface NavItem {
   label: string
   subLabel?: string
   children?: Array<NavItem>
-  href: string
+  href: string,
+  width: string
 }
 
 const NAV_ITEMS: Array<NavItem> = [
     {
       label: 'Dashboard',
       href: '/layout/dashboard',
+      width: '140px',
     },
     {
       label: 'Donation History',
       href: '/layout/donation-history',
+      width: '190px',
     },
     {
       label: 'Account Management',
       href: '/layout/account-management',
+      width: '210px',
     },
   ]
-
-const Tabs = () => {
-    return (
-      <Stack m='20px' direction='row' spacing={1} justify="space-between" flex="1" width="100%">
-        {NAV_ITEMS.map((navItem) => (
-          <Box key={navItem.label}>
-            <Link
-              as={RouterLink}
-              m='10px'
-              to={navItem.href}
-              px={2}
-              py={1}
-              rounded='md'
-              fontSize='20px'
-              color='#000000'
-              _hover={{
-                textDecoration: 'none',
-                bg: 'gray.200',
-              }}
-            >
-              {navItem.label}
-            </Link>
-          </Box>
-        ))}
-      </Stack>
-    )
-  }  
 
 export default function Navbar() {
   return (
@@ -66,27 +42,42 @@ export default function Navbar() {
         bg={useColorModeValue('white', 'gray.800')}
         color={useColorModeValue('gray.600', 'white')}
         minH='104px'
+        minW='1100px'
+        gap='16px'
         justify={{ base: 'center'}}
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderBottom={1}
+        justifyContent='space-between'
         borderStyle='solid'
         borderColor={useColorModeValue('gray.200', 'gray.900')}
-        align='center'>
-        <Flex flex={{ base: 1 }} justify='flex-end' alignItems='center'>
-          <Text align='center' alignItems='center'>
-            <Text as="span" color="#000000" fontWeight="bold" fontSize="30px">Don </Text>
-            <Text as="span" color="#A5154C" fontWeight="bold" fontSize="30px">Efficace.</Text>
-          </Text>
-          <Flex m='20px' flex={{ base: 1 }} justify={{ base: 'center', md: 'space-between' }} display={{ base: 'none', md: 'flex' }}>
-            <Tabs />  
-          </Flex>
-        </Flex>
-        <Stack
-          flex={{ base: 1, md: 0 }}
-          justify='flex-end'
-          direction='row'
-          spacing={6}>
+        align='center'
+        alignItems='center'>
+        <Text align='center' mb='12px' alignItems='center' pl='20px' minW='230px'>
+          <Text as="span" color="#000000" fontWeight="bold" fontSize="30px">Don </Text>
+          <Text as="span" color="#A5154C" fontWeight="bold" fontSize="30px">Efficace.</Text>
+        </Text>
+        {NAV_ITEMS.map((navItem) => (
+          <Link
+            key=''
+            as={RouterLink}
+            m='20px'
+            to={navItem.href}
+            fontSize='20px'
+            color='#000000'
+            minW={navItem.width}
+            _hover={{
+              textDecoration: "underline",
+              color: '#A5154C',
+              cursor: 'pointer'
+            }}
+            _focus={{ outline: "none" }}
+            _active={{ outline: "none" }}
+          >
+            {navItem.label}
+          </Link>
+        ))}
+        <Text align='center' minW='150px' alignItems='center' pr='30px'>
           <RouterLink to="/donate">
             <Button
               as='a'
@@ -94,18 +85,19 @@ export default function Navbar() {
               height='50px'
               display={{ base: 'center', md: 'left' }}
               fontSize='20px'
+              ml='20px'
               fontWeight={600}
-              cursor='pointer'
               color='#FFFFFF'
               bg='#A5154C'
               _hover={{
                 bg: '#A5154C',
-                color: '#FFFFFF'
+                color: '#FFFFFF',
+                cursor: 'pointer'
               }}>
               Donate Now
             </Button>
           </RouterLink>
-        </Stack>
+        </Text>
       </Flex>
     </Box>
   )
