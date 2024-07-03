f73c5e4bcad7694fea72358556f553393c6caa89
aayushh-patell <aayushptl2005@gmail.com>
2024-07-03 17:21:56 -0500

    Create Total Value Donated Component
    This is just the front-end component. We still have to fetch data from the database.

--- frontend/src/components/common/Layout.tsx
+++ frontend/src/components/common/Layout.tsx
@@ -4,6 +4,7 @@ import Navbar from './NavBar';
 import Dashboard from '../temp_navbar/Dashboard';
 import AccountManagement from '../temp_navbar/AccountManagement';
 import DonationHistory from '../temp_navbar/DonationHistory';
+import TotalValueDonatedBox from './TotalValueDonated';
 
 const Layout = () => {
     return (
@@ -16,6 +17,11 @@ const Layout = () => {
             <Route path="/layout/account-management" component={AccountManagement} />
             </Switch>
         </Box>
+        <TotalValueDonatedBox
+            title="Total Value Donated"
+            totalValueDonated={1000}
+            text="since your first donation"
+        />
         </Box>
     );
     };
---
+++ frontend/src/components/common/TotalValueDonated.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Box, Flex, Text, Heading, useColorModeValue } from '@chakra-ui/react'
+
+interface TotalValueDonatedProps {
+    title: string,
+    totalValueDonated: number,
+    text: string
+}
+
+const TotalValueDonatedBox: React.FC<TotalValueDonatedProps> = ({ title, totalValueDonated, text }) => {
+    return (
+      <Box border='1px solid' borderColor='gray.200' borderRadius='md' p={4} bg={useColorModeValue('white', 'gray.700')} boxShadow='sm' width='25%'>
+        <Heading as='h3' size='sm' mb={2} textAlign='center' margin='0px' color={useColorModeValue('gray.800', 'white')}>
+            {title}
+        </Heading>
+        
+        <Flex align='center' justify='space-between' borderRadius='md' p={2} mb={2} padding='20px 50px' margin='0px'>
+            <Text fontSize='2xl' fontWeight='bold' mb={2} margin='0px' color={useColorModeValue('gray.800', 'white')}>
+                ${totalValueDonated}
+            </Text>
+            <Text fontSize='sm' width='40%' margin='0px' color={useColorModeValue('gray.600', 'gray.200')}>
+                {text}
+            </Text>
+        </Flex>
+      </Box>
+    )
+}
+  
+export default TotalValueDonatedBox
