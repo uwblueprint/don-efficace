2459f8cc152db98b9aa9d441f637769663a5ab9a
Rosanne Zhu <110139243+roskzhu@users.noreply.github.com>
2024-10-03 21:23:19 -0400

    component done (#63)

--- frontend/src/components/temp_navbar/Donate.tsx
+++ frontend/src/components/temp_navbar/Donate.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import SelectCauseCheckbox from "../common/SelectCauseCheckbox";
 
 const Donate: React.FC = () => {
-  return <div>Donate Now!!!</div>;
+  return <div>
+    {/* pass in causes here */}
+    <SelectCauseCheckbox label="Cause [TO DO]" onChange={(checked) => console.log(checked)} />
+    Donate Now!!!</div>;
 };
 
 export default Donate;
