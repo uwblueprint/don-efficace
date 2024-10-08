a3fc961187e4cd025fd4cd4b54414f819f064261
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2024-10-07 22:27:28 -0400

    Hardcoded country list

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/no-static-element-interactions": "off",
     "react/prop-types": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
   },
   ignorePatterns: ["build/*"],
 };
