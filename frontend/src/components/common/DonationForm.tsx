ae2c0133220713d2754f3b396b73d33ee9bc876f
Victor Wei <victor@Victors-MacBook-Pro.local>
2024-10-17 19:12:50 -0400

    combined Donate and DonationHistory components

--- frontend/src/components/common/DonationForm.tsx
+++ frontend/src/components/common/DonationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Box,
   Image,
@@ -8,10 +8,13 @@ import {
   Checkbox,
   HStack,
   Icon,
-} from '@chakra-ui/react';
-import { InfoIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { InfoIcon } from "@chakra-ui/icons";
+import StripeCheckout from "../pages/StripeCheckout";
+import SelectCauseCheckbox from "./SelectCauseCheckbox";
+import heroBanner from "../../assets/hero-banner.jpeg";
 
-type Cause = 'health' | 'education' | 'animal';
+type Cause = "health" | "education" | "animal";
 
 const DonationForm: React.FC = () => {
   const [selectedCauses, setSelectedCauses] = useState({
@@ -28,26 +31,60 @@ const DonationForm: React.FC = () => {
   };
 
   return (
-    <Box display="flex" justifyContent="center" flexDirection="column" width="100%">
-      <Box display="flex" justifyContent="center" flexDirection="column" width="100%">
-      {/* Hero Banner */}
-      <Box width="100%" height="500px" position="relative" overflow="hidden">
-        <Image
-          position="absolute"
-          top="0"
-          left="0"
-          src="https://cdn.discordapp.com/attachments/1290454151700152390/1290454199058042941/DE-banner-photo.jpeg?ex=6705bf20&is=67046da0&hm=d668813c25e7a2a759ebb22510290c942d3e62cb56ae351acdb6e0b503323a0f&"
-          zIndex="1"
-          width="100%"
-          height="100%"
-          objectFit="cover" // This ensures the image maintains its aspect ratio
-        />
-        <Box position="absolute" top="0" left="0" backgroundColor="rgba(166, 22, 75, 0.5)" zIndex="2" width="100%" height="100%">&nbsp;</Box>
-        <Heading position="absolute" top="0" left="0" color="#FFF" fontSize="36pt" textAlign="center" zIndex="3" width="100%" height="100%" display="flex" alignItems="center" justifyContent="center" flexDirection="column">
-          Je veux m&apos;engager par mes dons.
-        </Heading>
+    <Box
+      display="flex"
+      justifyContent="center"
+      flexDirection="column"
+      width="100%"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        width="100%"
+      >
+        {/* Hero Banner */}
+        <Box width="100%" height="500px" position="relative" overflow="hidden">
+          <Image
+            position="absolute"
+            top="0"
+            left="0"
+            src={heroBanner}
+            zIndex="1"
+            width="100%"
+            height="100%"
+            objectFit="cover" // This ensures the image maintains its aspect ratio
+          />
+          <Box
+            position="absolute"
+            top="0"
+            left="0"
+            backgroundColor="rgba(166, 22, 75, 0.5)"
+            zIndex="2"
+            width="100%"
+            height="100%"
+          >
+            &nbsp;
+          </Box>
+          <Heading
+            position="absolute"
+            top="0"
+            left="0"
+            color="#FFF"
+            fontSize="36pt"
+            textAlign="center"
+            zIndex="3"
+            width="100%"
+            height="100%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            Je veux m&apos;engager par mes dons.
+          </Heading>
+        </Box>
       </Box>
-    </Box>
 
       <Box padding="5%">
         <Box margin="0px 0px 20px 0px">
@@ -58,9 +95,20 @@ const DonationForm: React.FC = () => {
 
         <Box width="80%" display="flex" marginLeft="5px">
           <FormControl id="cause" mb={4}>
-            <FormLabel fontSize="14pt" fontWeight="bold" display="flex" alignItems="center" margin="0px 0px 10px 0px">
+            <FormLabel
+              fontSize="14pt"
+              fontWeight="bold"
+              display="flex"
+              alignItems="center"
+              margin="0px 0px 10px 0px"
+            >
               Select Cause
-              <Icon as={InfoIcon} color="black.500" title="More information about causes" margin="0px 0px 3px 8px"/>
+              <Icon
+                as={InfoIcon}
+                color="black.500"
+                title="More information about causes"
+                margin="0px 0px 3px 8px"
+              />
             </FormLabel>
 
             <HStack display="flex" justifyContent="left" alignItems="center">
@@ -74,16 +122,18 @@ const DonationForm: React.FC = () => {
               >
                 <Checkbox
                   isChecked={selectedCauses.health}
-                  onChange={() => handleCheckboxChange('health')}
+                  onChange={() => handleCheckboxChange("health")}
                   padding="10px 12px 4px 12px"
                   sx={{
-                    '.chakra-checkbox__control': {
-                      borderColor: '#A6164B',
-                      backgroundColor: selectedCauses.health ? '#A6164B' : 'transparent',
+                    ".chakra-checkbox__control": {
+                      borderColor: "#A6164B",
+                      backgroundColor: selectedCauses.health
+                        ? "#A6164B"
+                        : "transparent",
                     },
-                    '.chakra-checkbox__control[data-checked]': {
-                      backgroundColor: '#A6164B',
-                      borderColor: '#A6164B',
+                    ".chakra-checkbox__control[data-checked]": {
+                      backgroundColor: "#A6164B",
+                      borderColor: "#A6164B",
                     },
                   }}
                 >
@@ -100,16 +150,18 @@ const DonationForm: React.FC = () => {
               >
                 <Checkbox
                   isChecked={selectedCauses.education}
-                  onChange={() => handleCheckboxChange('education')}
+                  onChange={() => handleCheckboxChange("education")}
                   padding="10px 12px 4px 12px"
                   sx={{
-                    '.chakra-checkbox__control': {
-                      borderColor: '#A6164B',
-                      backgroundColor: selectedCauses.education ? '#A6164B' : 'transparent',
+                    ".chakra-checkbox__control": {
+                      borderColor: "#A6164B",
+                      backgroundColor: selectedCauses.education
+                        ? "#A6164B"
+                        : "transparent",
                     },
-                    '.chakra-checkbox__control[data-checked]': {
-                      backgroundColor: '#A6164B',
-                      borderColor: '#A6164B',
+                    ".chakra-checkbox__control[data-checked]": {
+                      backgroundColor: "#A6164B",
+                      borderColor: "#A6164B",
                     },
                   }}
                 >
@@ -126,16 +178,18 @@ const DonationForm: React.FC = () => {
               >
                 <Checkbox
                   isChecked={selectedCauses.animal}
-                  onChange={() => handleCheckboxChange('animal')}
+                  onChange={() => handleCheckboxChange("animal")}
                   padding="10px 12px 4px 12px"
                   sx={{
-                    '.chakra-checkbox__control': {
-                      borderColor: '#A6164B',
-                      backgroundColor: selectedCauses.animal ? '#A6164B' : 'transparent',
+                    ".chakra-checkbox__control": {
+                      borderColor: "#A6164B",
+                      backgroundColor: selectedCauses.animal
+                        ? "#A6164B"
+                        : "transparent",
                     },
-                    '.chakra-checkbox__control[data-checked]': {
-                      backgroundColor: '#A6164B',
-                      borderColor: '#A6164B',
+                    ".chakra-checkbox__control[data-checked]": {
+                      backgroundColor: "#A6164B",
+                      borderColor: "#A6164B",
                     },
                   }}
                 >
@@ -146,6 +200,12 @@ const DonationForm: React.FC = () => {
           </FormControl>
         </Box>
       </Box>
+
+      <StripeCheckout />
+      <SelectCauseCheckbox
+        label="Cause [TO DO]"
+        onChange={(checked) => console.log(checked)}
+      />
     </Box>
   );
 };
